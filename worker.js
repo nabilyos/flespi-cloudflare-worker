74535b8a21a37be2e1e19c8e0916114328076513
nabilyos <yosnabil@gmail.com>
2025-12-31 11:01:07 +0100

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,24 @@
+export default {
+  async fetch(request, env) {
+    if (request.method !== "POST") {
+      return new Response("Only POST allowed", { status: 405 })
+    }
+
+    let data
+    try {
+      data = await request.json()
+    } catch {
+      return new Response("Invalid JSON", { status: 400 })
+    }
+
+    await fetch(env.FORWARD_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+
+    return new Response("OK", { status: 200 })
+  }
+}
